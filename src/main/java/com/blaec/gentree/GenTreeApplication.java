49afb4c3abfb83ecc4373d95bdddeb119703de08
blaec <odeskonst@yandex.ru>
2024-02-16 19:50:04 +0200

    first commit

---
+++ src/main/java/com/blaec/gentree/GenTreeApplication.java
@@ -0,0 +1,13 @@
+package com.blaec.gentree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenTreeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GenTreeApplication.class, args);
+    }
+
+}
